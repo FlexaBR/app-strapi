91444fac52e94b6f6b530655f65177c5623b2343
Ricardo Gutheil <r.gutheil@terra.com.br>
2020-11-18 19:43:52 -0300

    api-strapi-pg

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b3929e37f8ec624844cb54ed79f5ab46'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '22083209-e6ad-4b2b-8f97-2066da0141b9'
+};
